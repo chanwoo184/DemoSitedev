9a27e1f3f069a5da96031c7b0a1ce785a23d85d0
Chanwoo Lim <dlacksdn86@naver.com>
2024-11-20 14:02:05 +0900

    feat: 로그인/회원가입 성공 메시지

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router'; // 라우터 가져오기
 import '@/style.css' // style.css 파일을 불러옴
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
 
 const app = createApp(App);
 
 app.use(router); // 라우터 사용
+app.use(Toast, { // Toastification 플러그인 추가 (로그인/회원가입 성공 시 메시지를 화면에 보여줌)
+  position: 'top-right',
+  timeout: 2000,
+  closeOnClick: true,
+  pauseOnHover: true,
+}); 
 app.mount('#app');
